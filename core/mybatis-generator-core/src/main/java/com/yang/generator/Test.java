d86088c9e1edd81c588470b49ae898897b4bb98e
shyang <shy935397008@users.noreply.github.com>
2015-01-22 20:22:03 +0800

    add gen

--- core/mybatis-generator-core/src/main/java/com/yang/generator/Test.java
+++ core/mybatis-generator-core/src/main/java/com/yang/generator/Test.java
@@ -25,23 +25,24 @@ import freemarker.template.Template;
 import freemarker.template.TemplateException;
 
 public class Test {
-	private static final String CONSTANT="constant";
-	private static final String BEAN="bean";
-	private static final String CLASSNAME="className";
-	private static final String PROPERTITY="propertity";
-	private static final String VALUE="value";
-	public static void parse(List<Constant> listC,String fileName)
+	private static final String CONSTANT = "constant";
+	private static final String BEAN = "bean";
+	private static final String CLASSNAME = "className";
+	private static final String PROPERTITY = "propertity";
+	private static final String VALUE = "value";
+
+	public static void parseBean(List<Constant> listC, String fileName)
 			throws ParserConfigurationException, SAXException, IOException {
 		DocumentBuilder builder = DocumentBuilderFactory.newInstance()
 				.newDocumentBuilder();
 		Document document = builder.parse(Test.class.getClassLoader()
 				.getResourceAsStream(fileName));
 		Element element = document.getDocumentElement();
-		NodeList nodeList = null ;
-		if(fileName.contains(CONSTANT)){
+		NodeList nodeList = null;
+		if (fileName.contains(CONSTANT)) {
 			nodeList = element.getElementsByTagName(CONSTANT);
-			
-		}else if(fileName.contains(BEAN)){
+
+		} else if (fileName.contains(BEAN)) {
 			nodeList = element.getElementsByTagName(BEAN);
 		}
 		for (int i = 0; i < nodeList.getLength(); i++) {
@@ -52,9 +53,10 @@ public class Test {
 			Constant con = new Constant();
 			String string = item.getAttributes().getNamedItem(CLASSNAME)
 					.getTextContent();
-			if(string!=null&&string.trim().length()>0 ){
-				String pack=string.substring(0,string.lastIndexOf('.'));
-				String className=string.substring(string.lastIndexOf('.')+1);
+			if (string != null && string.trim().length() > 0) {
+				String pack = string.substring(0, string.lastIndexOf('.'));
+				String className = string
+						.substring(string.lastIndexOf('.') + 1);
 				con.setClassName(className);
 				con.setPack(pack);
 			}
@@ -70,7 +72,7 @@ public class Test {
 								.getTextContent());
 						p.setPropertity(attributes.getNamedItem(PROPERTITY)
 								.getTextContent());
-						if(fileName.contains(CONSTANT)){
+						if (fileName.contains(CONSTANT)) {
 							p.setValue(attributes.getNamedItem(VALUE)
 									.getTextContent());
 						}
@@ -82,39 +84,49 @@ public class Test {
 			listC.add(con);
 		}
 	}
-	
-	public static Template getTemplate(String fileName) throws IOException{
-		String file = Test.class.getClassLoader().getResource(fileName).getFile().substring(1);
-		String dir=file.substring(0,file.lastIndexOf("/"));
-		Configuration conf=new Configuration();
+
+	public static Template getTemplate(String fileName) throws IOException {
+		String file = Test.class.getClassLoader().getResource(fileName)
+				.getFile().substring(1);
+		String dir = file.substring(0, file.lastIndexOf("/"));
+		Configuration conf = new Configuration();
 		conf.setDirectoryForTemplateLoading(new File(dir));
 		conf.setObjectWrapper(new DefaultObjectWrapper());
 		return conf.getTemplate(fileName);
 	}
-	public static void genBean(String fileName,String tmpName) throws ParserConfigurationException, SAXException, IOException, TemplateException{
-		List<Constant> listC=new ArrayList<Constant>();
-		parse(listC,fileName);
-		for (Constant constant : listC) {
-			String dir=constant.getPack().replaceAll(File.separator+".", File.separator+File.separator);
-			File dirs=new File(dir);
-			if(!dirs.exists()){
-				dirs.mkdirs();
+
+	public static void genBean(String fileName, String tmpName)
+			throws ParserConfigurationException, SAXException, IOException,
+			TemplateException {
+		List<Constant> listC = new ArrayList<Constant>();
+		if (fileName.contains(CONSTANT)||fileName.contains(BEAN)) {
+			parseBean(listC, fileName);
+			for (Constant constant : listC) {
+				String dir = constant.getPack().replaceAll(File.separator + ".",
+						File.separator + File.separator);
+				File dirs = new File(dir);
+				if (!dirs.exists()) {
+					dirs.mkdirs();
+				}
+				String filePath = dir + File.separator + constant.getClassName()
+						+ ".java";
+				File f = new File(filePath);
+				Template template = getTemplate(tmpName);
+				Map<String, Object> map = new HashMap<String, Object>();
+				map.put("package", constant.getPack());
+				map.put("className", constant.getClassName());
+				map.put("propertites", constant.getList());
+				FileWriter writer = new FileWriter(f);
+				template.process(map, writer);
+				writer.flush();
+				writer.close();
 			}
-			String filePath=dir+File.separator+constant.getClassName()+".java";
-			File f=new File(filePath);
-			Template template = getTemplate(tmpName);
-			Map<String,Object> map=new HashMap<String,Object>();
-			map.put("package", constant.getPack());
-			map.put("className", constant.getClassName());
-			map.put("propertites", constant.getList());
-			FileWriter writer=new FileWriter(f);
-			template.process(map, writer);
-			writer.flush();
-			writer.close();
 		}
 	}
-	public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException, TemplateException {
-		genBean("constant.xml","constant.ftl");
-		genBean("bean.xml","bean.ftl");
+
+	public static void main(String[] args) throws ParserConfigurationException,
+			SAXException, IOException, TemplateException {
+		genBean("constant.xml", "constant.ftl");
+		genBean("bean.xml", "bean.ftl");
 	}
 }
