9391c0b2fc9f6b5a99fc36bdcf443e4b93ae4d95
shyang <shy935397008@users.noreply.github.com>
2015-01-27 23:52:11 +0800

    Update Test.java

--- core/mybatis-generator-core/src/main/java/com/yang/generator/Test.java
+++ core/mybatis-generator-core/src/main/java/com/yang/generator/Test.java
@@ -1,12 +1,20 @@
 package com.yang.generator;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -19,24 +27,54 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
+import com.yang.core.AbstractBean;
+import com.yang.core.Constant;
+import com.yang.core.Property;
+
 import freemarker.template.Configuration;
 import freemarker.template.DefaultObjectWrapper;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
 
-public class Test {
+public class GenTest {
 	private static final String CONSTANT = "constant";
 	private static final String BEAN = "bean";
+
 	private static final String CLASSNAME = "className";
-	private static final String PROPERTITY = "propertity";
+	private static final String PROPERTY = "property";
+	private static final String PROPERTITES = "properties";
 	private static final String VALUE = "value";
 
+	private static final String PACKAGE = "package";
+	private static final String IMPORT = "import";
+	private static final String JAVA = "java";
+
+	private static final String DOT = ".";
+	private static final String UNBIAS = "\\";
+
+	private static final String CONSTANTTMP = "constant.ftl";
+	private static final String BEANTMP = "bean.ftl";
+	private static final String DAOTMP = "dao.ftl";
+	private static final String SERVICETMP = "service.ftl";
+	private static final String CONTROLLERTMP = "controller.ftl";
+
+	private static final String DAO = "dao";
+	private static final String SERVICE = "service";
+	private static final String CONTROLLER = "controller";
+
+	/**
+	 * 解析配置文件
+	 * @param listC
+	 * @param fileName
+	 * @throws ParserConfigurationException
+	 * @throws SAXException
+	 * @throws IOException
+	 */
 	public static void parseBean(List<Constant> listC, String fileName)
 			throws ParserConfigurationException, SAXException, IOException {
 		DocumentBuilder builder = DocumentBuilderFactory.newInstance()
 				.newDocumentBuilder();
-		Document document = builder.parse(Test.class.getClassLoader()
-				.getResourceAsStream(fileName));
+		Document document = builder.parse(fileName);
 		Element element = document.getDocumentElement();
 		NodeList nodeList = null;
 		if (fileName.contains(CONSTANT)) {
@@ -54,23 +92,31 @@ public class Test {
 			String string = item.getAttributes().getNamedItem(CLASSNAME)
 					.getTextContent();
 			if (string != null && string.trim().length() > 0) {
-				String pack = string.substring(0, string.lastIndexOf('.'));
+				String pack = string.substring(0, string.lastIndexOf(DOT));
 				String className = string
-						.substring(string.lastIndexOf('.') + 1);
+						.substring(string.lastIndexOf(DOT) + 1);
 				con.setClassName(className);
 				con.setPack(pack);
 			}
-			List<Propertity> listP = new ArrayList<Propertity>();
+			List<Property> listP = new ArrayList<Property>();
+			Set<String> listImp = new HashSet<String>();
 			NodeList list = item.getChildNodes();
 			for (int j = 0; j < list.getLength(); j++) {
 				Node node = list.item(j);
-				Propertity p = new Propertity();
+				Property p = new Property();
 				if (node != null) {
 					NamedNodeMap attributes = node.getAttributes();
 					if (attributes != null) {
-						p.setClassName(attributes.getNamedItem(CLASSNAME)
-								.getTextContent());
-						p.setPropertity(attributes.getNamedItem(PROPERTITY)
+						String clazz = attributes.getNamedItem(CLASSNAME)
+								.getTextContent();
+						if (clazz.contains(DOT)) {
+							p.setClassName(clazz.substring(clazz
+									.lastIndexOf(DOT) + 1));
+							listImp.add(clazz);
+						} else {
+							p.setClassName(clazz);
+						}
+						p.setProperty(attributes.getNamedItem(PROPERTY)
 								.getTextContent());
 						if (fileName.contains(CONSTANT)) {
 							p.setValue(attributes.getNamedItem(VALUE)
@@ -80,53 +126,218 @@ public class Test {
 					}
 				}
 			}
+			con.setImports(listImp);
 			con.setList(listP);
 			listC.add(con);
 		}
 	}
 
+	/**
+	 * 获取模板
+	 * @param fileName 模板名
+	 * @return
+	 * @throws IOException
+	 */
 	public static Template getTemplate(String fileName) throws IOException {
-		String file = Test.class.getClassLoader().getResource(fileName)
-				.getFile().substring(1);
-		String dir = file.substring(0, file.lastIndexOf("/"));
 		Configuration conf = new Configuration();
-		conf.setDirectoryForTemplateLoading(new File(dir));
+		conf.setClassForTemplateLoading(Test.class, "");
 		conf.setObjectWrapper(new DefaultObjectWrapper());
 		return conf.getTemplate(fileName);
 	}
 
-	public static void genBean(String fileName, String tmpName)
-			throws ParserConfigurationException, SAXException, IOException,
-			TemplateException {
+	public static void genBean(String url) throws ParserConfigurationException,
+			SAXException, IOException, TemplateException {
 		List<Constant> listC = new ArrayList<Constant>();
-		if (fileName.contains(CONSTANT)||fileName.contains(BEAN)) {
-			parseBean(listC, fileName);
+		if (url.contains(CONSTANT) || url.contains(BEAN)) {
+			parseBean(listC, url);
 			for (Constant constant : listC) {
-				String dir = constant.getPack().replaceAll(File.separator + ".",
+				// ----------------------1.bean--------------------------
+				String dir = constant.getPack().replaceAll(UNBIAS + DOT,
 						File.separator + File.separator);
-				File dirs = new File(dir);
-				if (!dirs.exists()) {
-					dirs.mkdirs();
+				String[] split = constant.getPack().split("\\.");
+				String baseDir = dir.substring(0,
+						dir.lastIndexOf(File.separator));
+				String beanDir = null;
+				if (url.contains(BEAN)) {
+					beanDir = baseDir + File.separator
+							+ constant.getPack().split("\\.")[split.length - 1];
+				} else if (url.contains(CONSTANT)) {
+					beanDir = baseDir + File.separator + CONSTANT;
 				}
-				String filePath = dir + File.separator + constant.getClassName()
-						+ ".java";
+				genDir(beanDir);
+				String filePath = beanDir + File.separator
+						+ constant.getClassName() + DOT + JAVA;
 				File f = new File(filePath);
-				Template template = getTemplate(tmpName);
 				Map<String, Object> map = new HashMap<String, Object>();
-				map.put("package", constant.getPack());
-				map.put("className", constant.getClassName());
-				map.put("propertites", constant.getList());
-				FileWriter writer = new FileWriter(f);
-				template.process(map, writer);
-				writer.flush();
-				writer.close();
+				map.put(PACKAGE, constant.getPack());
+				map.put(CLASSNAME, constant.getClassName());
+				map.put(PROPERTITES, constant.getList());
+				map.put(IMPORT, constant.getImports());
+				if (url.contains(CONSTANT)) {
+					genSource(map, CONSTANTTMP, f);
+				} else if (url.contains(BEAN)) {
+					genSource(map, BEANTMP, f);
+				}
+				if (url.contains(BEAN)) {
+					// --------------------------2. dao-------------------
+
+					List<String> classList = new ArrayList<String>();
+					String beanClass = constant.getPack() + DOT
+							+ constant.getClassName();
+					classList.add(beanClass);
+					String daoClass = genCode(baseDir, map, constant, DAO,
+							DAOTMP, classList);
+
+					// --------------------------3. service--------------------
+					classList.add(beanClass);
+					classList.add(daoClass);
+					String ServiceClass = genCode(baseDir, map, constant,
+							SERVICE, SERVICETMP, classList);
+					// -----------------------------4
+					// controller----------------------------------------------
+					classList.add(beanClass);
+					classList.add(ServiceClass);
+					genCode(baseDir, map, constant, CONTROLLER, CONTROLLERTMP,
+							classList);
+
+					// --------------------------------------------5.VIEW----------------------------------------------------------------
+					// TODO GEN HTML
+				} else if (url.contains(CONSTANT)) {
+					// properties 常量
+					String cons = baseDir + File.separator + CONSTANT;
+					genDir(cons);
+					List<? extends Property> list = constant.getList();
+					Properties p = new Properties();
+					for (Property pro : list) {
+						p.put(constant.getPack() + DOT
+								+ constant.getClassName() + DOT
+								+ pro.getProperty(), pro.getValue());
+					}
+					File constantFile = new File(cons + File.separator
+							+ constant.getClassName() + DOT + PROPERTITES);
+					OutputStream os = new FileOutputStream(constantFile);
+					p.store(os, constant.getClassName() + " property");
+					os.flush();
+					os.close();
+				}
 			}
 		}
 	}
 
+	public static void genFromFile(String file)
+			throws ParserConfigurationException, SAXException, IOException,
+			TemplateException {
+		File f = new File(file);
+		genBean(f.toURI().toString());
+	}
+
+	/**
+	 * 首字母大写
+	 * @param str
+	 * @return
+	 */
+	public static String firstUpper(String str) {
+		char[] cs = str.toCharArray();
+		cs[0] -= 32;
+		return new String(cs);
+	}
+
+	/**
+	 * 文件夹生成
+	 * @param dir 文件夹
+	 */
+	private static void genDir(String dir) {
+		if (dir != null && !"".equals(dir.trim())) {
+			File f = new File(dir);
+			if (!f.exists()) {
+				f.mkdirs();
+			}
+		}
+	}
+
+	/**
+	 * 根据模板生成文件
+	 * @param map 数据 model
+	 * @param tmpName 模板名称
+	 * @param f 要生成的文件
+	 * @throws IOException
+	 * @throws TemplateException
+	 */
+	public static void genSource(Map<String, Object> map, String tmpName, File f)
+			throws IOException, TemplateException {
+		Template template = getTemplate(tmpName);
+		FileWriter writer = new FileWriter(f);
+		template.process(map, writer);
+		writer.flush();
+		writer.close();
+	}
+
+	/**
+	 * 文件拷贝
+	 * @param fileName
+	 * @throws IOException
+	 */
+	public static void copy(String fileName) throws IOException {
+		String dir = System.getProperty("user.dir");
+		InputStream stream = Test.class.getClassLoader()
+				.getResourceAsStream(fileName);
+		BufferedOutputStream bos = new BufferedOutputStream(
+				new FileOutputStream(dir + File.separator + fileName));
+		BufferedInputStream bis = new BufferedInputStream(stream);
+		int len = 0;
+		byte[] buf = new byte[2048];
+		while ((len = bis.read(buf)) != -1) {
+			bos.write(buf, 0, len);
+		}
+		bos.flush();
+		bos.close();
+		bis.close();
+		stream.close();
+	}
+
+	/**
+	 * 用来生产dao或 service或 constroller
+	 * @param baseDir
+	 * @param map
+	 * @param constant
+	 * @param layer DAO/SERVICE/CONTROLLER
+	 * @param tmp 模板
+	 * @param list importList
+	 * @return 生成的类完全名
+	 * @throws IOException
+	 * @throws TemplateException
+	 */
+	public static String genCode(String baseDir, Map<String, Object> map,
+			AbstractBean constant, String layer, String tmp, List<String> list)
+			throws IOException, TemplateException {
+		String dir = baseDir + File.separator + layer;
+		genDir(dir);
+		File file = new File(dir + File.separator + constant.getClassName()
+				+ firstUpper(layer) + DOT + JAVA);
+		map.put(PACKAGE,
+				dir.replaceAll(File.separator + File.separator, UNBIAS + DOT));
+		map.put(IMPORT, list);
+		genSource(map, tmp, file);
+		list.clear();
+		return dir.replaceAll(File.separator + File.separator, UNBIAS + DOT)
+				+ DOT + constant.getClassName() + firstUpper(layer);
+	}
+
 	public static void main(String[] args) throws ParserConfigurationException,
 			SAXException, IOException, TemplateException {
-		genBean("constant.xml", "constant.ftl");
-		genBean("bean.xml", "bean.ftl");
+		if (args != null && args.length > 0) {
+			for (String str : args) {
+				if (str.endsWith(".xml")) {
+					genFromFile(str);
+				}
+			}
+		} else {
+			genBean(Test.class.getClassLoader().getResource("bean.xml")
+					.toString());
+			genBean(Test.class.getClassLoader().getResource("constant.xml")
+					.toString());
+			copy("bean.xml");
+			copy("constant.xml");
+		}
 	}
 }
