7d32d5cacdbbe639b25de3f50fe8aa6d3bd95bf7
shyang <shy935397008@users.noreply.github.com>
2015-01-28 09:52:36 +0800

    add

---
+++ core/mybatis-generator-core/src/main/java/com/yang/generator/AbstractProperty.java
@@ -0,0 +1,24 @@
+package com.yang.generator;
+
+public class AbstractProperty {
+
+	private String className;
+	private String property;
+
+	public String getClassName() {
+		return className;
+	}
+
+	public void setClassName(String className) {
+		this.className = className;
+	}
+
+	public String getProperty() {
+		return property;
+	}
+
+	public void setProperty(String property) {
+		this.property = property;
+	}
+	
+}
---
+++ core/mybatis-generator-core/src/main/java/com/yang/generator/Property.java
@@ -0,0 +1,13 @@
+package com.yang.generator;
+
+public class Property extends AbstractProperty{
+
+	private String value;
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+	
+}
