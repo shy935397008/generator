cb86dd4bdfab9dd2c185eec281e95536cd8609d7
shyang <shy935397008@users.noreply.github.com>
2015-01-28 09:51:32 +0800

    mod

--- core/mybatis-generator-core/src/main/java/com/yang/generator/AbstractBean.java
+++ core/mybatis-generator-core/src/main/java/com/yang/generator/AbstractBean.java
@@ -1,13 +1,16 @@
 package com.yang.generator;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class AbstractBean {
 
 	private String className;
 	private String pack;
-	private List<? extends AbstractPropertity> list = new ArrayList< AbstractPropertity>();
+	private List<? extends Property> list = new ArrayList<Property>();
+	private Set<String> imports=new HashSet<String>();
 
 	public String getClassName() {
 		return className;
@@ -25,11 +28,20 @@ public class AbstractBean {
 		this.pack = pack;
 	}
 
-	public List<? extends AbstractPropertity> getList() {
+	public List<? extends Property> getList() {
 		return list;
 	}
 
-	public void setList(List<? extends AbstractPropertity> list) {
+	public void setList(List<? extends Property> list) {
 		this.list = list;
 	}
+
+	public Set<String> getImports() {
+		return imports;
+	}
+
+	public void setImports(Set<String> imports) {
+		this.imports = imports;
+	}
+	
 }
