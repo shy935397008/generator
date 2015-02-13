e0c893dbce29e39c8f49134736433e128578e1c2
shyang <shy935397008@users.noreply.github.com>
2015-02-13 23:02:04 +0800

    model

--- core/mybatis-generator-core/src/main/java/com/yang/stock/bean/Stocks.java
+++ core/mybatis-generator-core/src/main/java/com/yang/stock/bean/Stocks.java
@@ -1,17 +1,17 @@
 package com.yang.stock.bean;
 
-public class Stocks {
+public class Stocks{
 
 	private String code;
 	private String name;
 	private String operation;
 	private String date;
-	private String id;
+	private int id;
 	
-	public String getId() {
+	public int getId() {
 		return id;
 	}
-	public void setId(String id) {
+	public void setId(int id) {
 		this.id = id;
 	}
 	public String getDate() {
@@ -38,4 +38,53 @@ public class Stocks {
 	public void setOperation(String operation) {
 		this.operation = operation;
 	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((code == null) ? 0 : code.hashCode());
+		result = prime * result + ((date == null) ? 0 : date.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result
+				+ ((operation == null) ? 0 : operation.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Stocks other = (Stocks) obj;
+		if (code == null) {
+			if (other.code != null)
+				return false;
+		} else if (!code.equals(other.code))
+			return false;
+		if (date == null) {
+			if (other.date != null)
+				return false;
+		} else if (!date.equals(other.date))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (operation == null) {
+			if (other.operation != null)
+				return false;
+		} else if (!operation.equals(other.operation))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Stocks [code=" + code + ", name=" + name + ", operation="
+				+ operation + ", date=" + date + "]";
+	}
+	
 }
