37777255cd269c47eff6c2d854d1bb6a78b3fd05
shyang <shy935397008@users.noreply.github.com>
2015-02-14 08:48:33 +0800

    mod

---
+++ core/mybatis-generator-core/src/main/java/com/yang/stock/bean/Stock.java
@@ -0,0 +1,15 @@
+package com.yang.stock.bean;
+
+public class Stock extends Stocks{
+
+	private String stockCode;
+
+	public String getStockCode() {
+		return stockCode;
+	}
+
+	public void setStockCode(String stockCode) {
+		this.stockCode = stockCode;
+	}
+	
+}
